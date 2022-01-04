ae582fd581ba437d251e94b2655aa119e6f07405
Sheetal Panda <sheetalpanda@Sheetals-MacBook-Pro.local>
2022-01-04 19:34:06 +0530

    [Sheetal] Add. overriden settongs method and created size method inside to change size of window

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -4,4 +4,9 @@ public class TryProcessing extends PApplet {
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
     }
+
+    @Override
+    public void settings() {
+        size(640,480);
+    }
 }
