657b6610a0b6e91c80b1f88b036055a974ba7381
Sheetal Panda <sheetalpanda@Sheetals-MacBook-Pro.local>
2022-01-04 21:20:26 +0530

    [Sheetal] Refactor. draw and setup method to ensure proper encapsulation

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -4,22 +4,15 @@ public class TryProcessing extends PApplet {
 
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
-    public static final int DIAMETER = 20;
     public static final int SPEED_1 = 1;
     public static final int SPEED_2 = 2;
     public static final int SPEED_3 = 3;
     public static final int SPEED_4 = 4;
-    private int x1 = 0;
-    private int x2 = 0;
-    private int x3 = 0;
-    private int x4 = 0;
 
-
-    Ball ball1 = new Ball(x1, HEIGHT / 4, SPEED_1);
-    Ball ball2 = new Ball(x2, HEIGHT * 2 / 4, SPEED_2);
-    Ball ball3 = new Ball(x3, HEIGHT * 3 / 4, SPEED_3);
-    Ball ball4 = new Ball(x4, HEIGHT * 4 / 4, SPEED_4);
-
+    Ball ball1;
+    Ball ball2;
+    Ball ball3;
+    Ball ball4;
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
@@ -34,19 +27,22 @@ public class TryProcessing extends PApplet {
     public void setup() {
         //ellipse(640/2, 480/2, 20, 20);
         //executes only once whereas draw method will get executes again and again in loop
+        ball1 = new Ball(0, HEIGHT / 4, SPEED_1);
+        ball2 = new Ball(0, HEIGHT * 2 / 4, SPEED_2);
+        ball3 = new Ball(0, HEIGHT * 3 / 4, SPEED_3);
+        ball4 = new Ball(0, HEIGHT * 4 / 4, SPEED_4);
     }
 
     @Override
     public void draw() {
-        ball1.setX(ball1.getX() + ball1.getSpeed());
-        ball2.setX(ball2.getX() + ball2.getSpeed());
-        ball3.setX(ball3.getX() + ball3.getSpeed());
-        ball4.setX(ball4.getX() + ball4.getSpeed());
+        ball1.move();
+        ball2.move();
+        ball3.move();
+        ball4.move();
 
-
-        ellipse(ball1.getX(), ball1.getY(), DIAMETER, DIAMETER);
-        ellipse(ball2.getX(), ball2.getY(), DIAMETER, DIAMETER);
-        ellipse(ball3.getX(), ball3.getY(), DIAMETER, DIAMETER);
-        ellipse(ball4.getX(), ball4.getY(), DIAMETER, DIAMETER);
+        ball1.draw(this);
+        ball2.draw(this);
+        ball3.draw(this);
+        ball4.draw(this);
     }
 }
