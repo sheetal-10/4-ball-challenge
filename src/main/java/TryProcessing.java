c7b35bef58cd66ed2f1d9019f412127bb557d178
Sheetal Panda <sheetalpanda@Sheetals-MacBook-Pro.local>
2022-01-04 20:02:18 +0530

    [Sheetal] Add. logic to create 4 balls and move that 4 balls

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -5,7 +5,15 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 20;
-    private int x=0;
+    public static final int SPEED_1 = 1;
+    public static final int SPEED_2 = 2;
+    public static final int SPEED_3 = 3;
+    public static final int SPEED_4 = 4;
+    private int x1 = 0;
+    private int x2 = 0;
+    private int x3 = 0;
+    private int x4 = 0;
+
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
@@ -19,12 +27,14 @@ public class TryProcessing extends PApplet {
     @Override
     public void setup() {
         //ellipse(640/2, 480/2, 20, 20);
-        //executes only once where as draw method will get executes again and again in loop
+        //executes only once whereas draw method will get executes again and again in loop
     }
 
     @Override
     public void draw() {
-        ellipse(x, HEIGHT/2, DIAMETER, DIAMETER);
-        x++;
+        ellipse(x1 += SPEED_1, HEIGHT / 4, DIAMETER, DIAMETER);
+        ellipse(x2 += SPEED_2, HEIGHT * 2 / 4, DIAMETER, DIAMETER);
+        ellipse(x3 += SPEED_3, HEIGHT * 3 / 4, DIAMETER, DIAMETER);
+        ellipse(x4 += SPEED_4, HEIGHT * 4 / 4, DIAMETER, DIAMETER);
     }
 }
